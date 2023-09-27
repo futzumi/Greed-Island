84ca6bbba078ed17abd9ce21eb5192c3fab2196f
futzumi <102948087+futzumi@users.noreply.github.com>
2023-09-27 11:36:07 -0400

    Update fetchSpecifiedCards.js

--- fetchSpecifiedCards.js
+++ fetchSpecifiedCards.js
@@ -1,10 +1,11 @@
 var Cards;
 fetch("https://raw.githubusercontent.com/futzumi/Greed-Island/main/Cards.json")
-  .then(function(response) { 
+  .then(function(response) {
     response.text().then(function(text) {
       Cards = JSON.parse(text);
       for (let i = 0; i < Cards.SpecifiedSlotCards.length; i++) {
-  document.body.innerHTML += Cards.SpecifiedSlotCards[i].split(";")[0] + "  " + Cards.SpecifiedSlotCards[i].split(";")[1] + "  " + Cards.SpecifiedSlotCards[i].split(";")[2] + "<br>"
-}
+        document.body.innerHTML += `<div class='wrapper'> <r>` + Cards.SpecifiedSlotCards[i].split(";")[0] + `</r> <t>` + Cards.SpecifiedSlotCards[i].split(";")[1] + `</t> <m>` + Cards.SpecifiedSlotCards[i].split(";")[2] + `</m>` + "" + `</div>`;   
+
+      }
     });
   });
